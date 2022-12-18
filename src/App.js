b136948b7db73843c55c815c3e5bc63287ddc412
Shivam Sharma <shivayasharma1149@gmail.com>
2022-12-18 23:26:53 +0530

    Warning Rmoved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './assets/css/index.css';
-import { Route, Routes } from "react-router-dom";
+import {
+  // Route,
+  Routes
+} from "react-router-dom";
 import Navigation from './components/navigation/Navigation';
 
 function App() {
   return (
     <div className="content-wrapper">
       <Routes>
-      
+
       </Routes>
       <Navigation />
     </div>
