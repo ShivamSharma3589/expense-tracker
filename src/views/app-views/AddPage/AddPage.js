2d72d1ae21587f73fea2ab433c97f515f5559057
Shivam Sharma <shivayasharma1149@gmail.com>
2022-12-20 23:58:20 +0530

    Added the Form Fields in the Add Page

--- src/views/app-views/AddPage/AddPage.js
+++ src/views/app-views/AddPage/AddPage.js
@@ -2,29 +2,58 @@ import React from 'react';
 import {
   Card,
   Tabs,
+  Form,
+  Input,
+  Button,
 } from 'antd';
-
-
-const items = new Array(3).fill(null).map((_, i) => {
-  const id = String(i + 1);
-  return {
-    label: `Tab Title ${id}`,
-    key: id,
-    children: (
-      <>
-        <p>Content of Tab Pane {id}</p>
-        <p>Content of Tab Pane {id}</p>
-        <p>Content of Tab Pane {id}</p>
-      </>
-    ),
-  };
-});
+const { Tabpane } = Tabs;
 
 const AddPage = () => {
   return (
     <section className='single-page'>
       <Card>
-        <Tabs type="card" items={items} />
+        {/* <Tabs type="card" items={items} /> */}
+        <Tabs type='card'>
+          <Tabpane key="1" tab="Add Amount">
+            <Form>
+              <Form.Item
+                label="Amount Holder Name: "
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Amount to be added: "
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item>
+                <Button type='primary' solid block>Submit</Button>
+              </Form.Item>
+            </Form>
+          </Tabpane>
+          <Tabpane key="2" tab="Add Expense">
+            <Form>
+              <Form.Item
+                label="Expense Name:"
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Amount:"
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Description:"
+              >
+                <Input.TextArea />
+              </Form.Item>
+              <Form.Item>
+                <Button type='primary' block>Submit</Button>
+              </Form.Item>
+            </Form>
+          </Tabpane>
+        </Tabs>
       </Card>
     </section>
   )
